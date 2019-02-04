10afb29a4408ba11e80fa279a834c46286640f75
preeteeshremalli <preeteesh.remalli@unilever.com>
2019-02-04 15:56:32 +0530

    Initial Commit

---
+++ ChatBotApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ChatBotApp
+//
+//  Created by Oneview Infosys on 04/02/19.
+//  Copyright © 2019 Oneview Infosys. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
