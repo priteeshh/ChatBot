dac70b19b2d8aa7a749ac13ef2fec19c15120445
preeteeshremalli <preeteesh.remalli@unilever.com>
2019-02-04 20:18:11 +0530

    pushing code

---
+++ ChatBotApp/MessageCell.swift
@@ -0,0 +1,24 @@
+//
+//  MessageCell.swift
+//  ChatBotApp
+//
+//  Created by Oneview Infosys on 04/02/19.
+//  Copyright © 2019 Oneview Infosys. All rights reserved.
+//
+
+import UIKit
+
+class MessageCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
